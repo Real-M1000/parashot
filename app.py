4a1d92b4b27ac6eb226c9ea0b880cd9a5c1c3f56
Real-M1000 <jonah@permakulturhof.de>
2026-03-29 20:18:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '<h1>Hallo von meinem Ubuntu-Server!</h1><p>Flask läuft im Docker-Container.</p>'
+@app.route('/dateien')
+def liste_dateien():
+    # Wir schauen in den Ordner, den wir via Volume gemountet haben
+    pfad = '/app/Files/BzT'
+    if os.path.exists(pfad):
+        dateien = os.listdir(pfad)
+        return f"Dateien auf dem Server: {', '.join(dateien)}"
+    else:
+        return "Ordner nicht gefunden!", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
